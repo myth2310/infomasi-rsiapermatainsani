d6010e59a0cd5590afe04a5f99a53a2334085bdc
myth2310 <achmadmiftahudin2310@gmail.com>
2025-07-29 11:14:47 +0700

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import JadwalDokter from './pages/JadwalDokter';
+import JadwalDokter from './pages/JadwalDokter.jsx';
 
 
 function App() {
